5c3d27f21b5fe336db2be26cb1923b61ce534c03
Isur <arturbed96@gmail.com>
2018-03-18 14:44:53 +0100

    Second lists. Maybe Done.

---
+++ 02.py
@@ -0,0 +1,119 @@
+from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+from sklearn import datasets
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import scipy.io as sio
+import random
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn import neighbors, datasets
+
+# Zadanie 1:
+banana_data = sio.loadmat('banana.mat')['test_data']
+banana_labels = sio.loadmat('banana.mat')['test_labels']
+
+train, test, train_targets, test_targets = train_test_split(banana_data, banana_labels,
+                                 test_size=0.30, random_state=42)
+counter = 0
+
+# Zadanie 2:
+gnb = GaussianNB()
+predicted =gnb.fit(train, np.ravel(train_targets)).predict(test)
+
+# Zadanie 3:
+x = []
+y = []
+z = []
+for i in range(len(test)):
+    x.append(test[i][0])
+    y.append(test[i][1])
+    z.append(predicted[i])
+
+for i in range(len(x)):
+    if z[i] == 1:
+        plt.scatter(x[i],y[i],c="RED")
+    else:
+        plt.scatter(x[i],y[i],c="GREEN")
+
+
+plt.show()
+# Zadanie 4:
+print("Classifier efficiency: %f" % gnb.score(train,np.ravel(train_targets)))
+# Zadanie 5:
+clf = NearestCentroid()
+
+# Zadanie 6:
+clf.fit(train,np.ravel(train_targets))
+predicted = clf.predict(test)
+# Zadanie 7:
+x = []
+y = []
+z = []
+c1 = [0,0]
+c2 = [0,0]
+for i in range(len(test)):
+    x.append(test[i][0])
+    y.append(test[i][1])
+    z.append(predicted[i])
+    if(z[i]==1):
+        c1[0]+=x[i]
+        c1[1]+=y[i]
+    else:
+        c2[0]+=x[i]
+        c2[1]+=y[i]
+
+counter1 = 0
+counter2 = 0
+for i in range(len(x)):
+    if z[i] == 1:
+        plt.scatter(x[i],y[i],c="RED")
+        counter1 += 1
+    else:
+        plt.scatter(x[i],y[i],c="GREEN")
+        counter2 += 1
+c1 = [c1[0]/counter1,c1[1]/counter1]
+c2 = [c2[0]/counter2,c2[1]/counter2]
+plt.scatter(c1[0],c1[1],c="BLUE")
+plt.scatter(c2[0],c2[1],c="BROWN")
+plt.show()
+
+# Zadanie 8:
+print("Classifier efficiency: %f" % clf.score(train,np.ravel(train_targets)))
+
+# Zadanie 9:
+k_best = [0,0]
+for k in range(5,17):
+    knn = neighbors.KNeighborsClassifier(k,weights='uniform', metric='euclidean')
+    knn.fit(train,np.ravel(train_targets))
+    predicted = knn.predict(test)
+    sc = knn.score(train,np.ravel(train_targets))
+    if sc > k_best[1]:
+        k_best = [k,sc]
+print("Best efficiency in 5-16 is for k =", k_best[0], " --> efficiency : ", k_best[1])
+
+# Zadanie 10:
+knn = neighbors.KNeighborsClassifier(k_best[0],weights='uniform', metric='euclidean')
+knn.fit(train,np.ravel(train_targets))
+predicted = knn.predict(test)
+sc = knn.score(train,np.ravel(train_targets))
+x = []
+y = []
+z = []
+for i in range(len(test)):
+    x.append(test[i][0])
+    y.append(test[i][1])
+    z.append(predicted[i])
+
+for i in range(len(x)):
+    if z[i] == 1:
+        plt.scatter(x[i],y[i],c="RED")
+    else:
+        plt.scatter(x[i],y[i],c="GREEN")
+plt.show()
+
+# Zadanie 11:
+print("Bad classify: ", int(len(x)*(1-sc)))
